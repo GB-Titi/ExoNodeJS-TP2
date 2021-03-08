ef3eaffd1cd8a011e745cd297e12459eba071684
GB-Titi <tristanmatteoli@gmail.com>
2021-03-08 16:47:54 +0100

    Ajout deuxieme route et commentaires pour des notes perso

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+//vidéos google explications -> HTTP203 (google developper)
+
 //SUJET TP:
 //Réaliser un chat en temps réel (sans persistances de données)
 //Le chat fonctionne en localhost et utiliser deux onglets de navigation pour simuler deux utilisateur du chat
 //lancer plein de requete pour vérifier que le code ne soit pas bloquant (synchrone)
 
 //pour utiliser socket.io --> npm i --save socket.io
+
+//recharger projet sur controle S --> "Forever" (ou process manager regarder sur google)
 
 //j'ai suivi le cours et donc pas encore commencé le tp (j'étais pas là l'année dernière)
 
@@ -15,6 +19,10 @@ const app = express(); //crée le serveur web avec express
 //on initialise la route de base (racine) avec "/" sur un get
 app.get('/', (req, res) =>{
     res.end('Hello World !');
+});
+
+app.get('/discussion', (req, res) =>{
+    res.end('Envoie moi ton message bg');
 });
 
 //methode qui permet de dire au serveur sur quel port écouter, on console log pour vérifier la connexion via un callback ('=>')
